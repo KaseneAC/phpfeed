a6b0925303a81d1005e8693840609c382a622b08
Kasene Clark <kaseneac@gmail.com>
2012-12-15 21:59:07 -0500

    Verson 2 (Stable)
    Gets an XML Feed and properly returns a formatted json block.

--- phpfeed.php
+++ phpfeed.php
@@ -2,29 +2,27 @@
 
 
 // Obtain Feed Url From POST
-$phpfeed_url = $_POST['phpfeedurl'];
+$phpfeed_url = $_GET['phpfeedurl'];
 
 
-if ($phpfeed_url) // Check to see if we actually got a URL.
+if ($phpfeed_url == "") // Check to see if we didn't get a URL.
 {
-	// If so continue with the operation.
-	continue;
-
-}else{
 	// Halt the script with a fatal error.
 	die("Fatal Error: phpfeed did not find a url...");
 }
 
+// Get The XML
+$file_contents = file_get_contents($phpfeed_url);
+
+//Prepare the string for xml parsing
+$file_contents = str_replace(array("\n", "\r", "\t"), '', $file_contents);
 
 // Create the xml object
-$phpfeed_xml = simplexml_load_file($phpfeed_url);
+$phpfeed_xml = simplexml_load_string($file_contents);
 
 // Get the item array
-$phpfeed_item_array = $phpfeed_xml->rss->channel->item;
-
-// Setup the result output
-$phpfeed_result = json_encode($phpfeed_item_array);
+$phpfeed_item_json = json_encode($phpfeed_xml);
 
 // Echo the output to the page/javascript
-echo $phpfeed_result;
+echo $phpfeed_item_json;
 
